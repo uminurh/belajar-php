7fd757f670197272941d4978c3ff1bb053cb0c17
Umi Nur Habibah <uminurhabibah2001@gmail.com>
2022-10-24 11:09:12 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "Hello World!" ;
+
+$nama = "Umi Nur Habibah";
+$umur = "20" ;
+
+echo "Nama Saya <strong>$nama</strong>, saya berusia $umur tahun." ;
+
+$namaKucing ="Mako";
+$umurKucing ="1";
+
+echo "Nama kucing saya <strong>$namaKucing</strong>, kucing saya masih berusia $umurKucing tahun.<br>";
+
+$selisihUmur = $umur - $umurKucing;
+
+echo "Selisih usia saya dan kucing saya adalah $selisihUmur tahun."
+
+
+
+?>
